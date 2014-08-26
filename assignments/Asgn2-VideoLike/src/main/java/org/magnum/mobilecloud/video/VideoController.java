15b1028b5409ad5bf62e23c266f2c4e8ce57f205
Michael Marcus <dev@michaelmarcus.net>
2014-08-25 21:24:57 -0400

    Assignment 2 - Implemented getVideo, likeVideo, unlikeVideo

--- assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/VideoController.java
+++ assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/VideoController.java
@@ -18,13 +18,17 @@
 
 package org.magnum.mobilecloud.video;
 
+import java.security.Principal;
 import java.util.Collection;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.magnum.mobilecloud.video.client.VideoSvcApi;
 import org.magnum.mobilecloud.video.repository.Video;
 import org.magnum.mobilecloud.video.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,38 +40,113 @@ import com.google.common.collect.Lists;
 @Controller
 public class VideoController {
 
+	private static final String ID_PARAM = "id";
+	private static final String VIDEO_PATH = VideoSvcApi.VIDEO_SVC_PATH;
+	private static final String VIDEO_ID_PATH = VIDEO_PATH + "/{" + ID_PARAM
+			+ "}";
+
 	@Autowired
 	private VideoRepository videos;
 
-	@RequestMapping(value = "/go", method = RequestMethod.GET)
-	public @ResponseBody String goodLuck() {
-		return "Good Luck!";
-	}
-
-	@RequestMapping(value = VideoSvcApi.VIDEO_SVC_PATH, method = RequestMethod.POST)
+	// POST /video
+	@RequestMapping(value = VIDEO_PATH, method = RequestMethod.POST)
 	public @ResponseBody Video addVideo(@RequestBody Video v) {
 		Video savedVideo = videos.save(v);
 		return savedVideo;
 	}
 
-	@RequestMapping(value = VideoSvcApi.VIDEO_SVC_PATH, method = RequestMethod.GET)
+	// GET /video
+	@RequestMapping(value = VIDEO_PATH, method = RequestMethod.GET)
 	public @ResponseBody Collection<Video> getVideoList() {
 		Iterable<Video> results = videos.findAll();
 		return Lists.newArrayList(results);
 	}
 
+	// GET /video/{id}
+	@RequestMapping(value = VIDEO_ID_PATH, method = RequestMethod.GET)
+	public @ResponseBody Video getVideo(@PathVariable(ID_PARAM) long id,
+			HttpServletResponse response) {
+
+		Video v = getVideoWithResponse(id, response);
+
+		return v;
+	}
+
+	// POST /video/{id}/like
+	@RequestMapping(value = VIDEO_ID_PATH + "/like", method = RequestMethod.POST)
+	public @ResponseBody void likeVideo(@PathVariable(ID_PARAM) long id,
+			Principal p, HttpServletResponse response) {
+
+		Video v = getVideoWithResponse(id, response);
+		if (v == null) {
+			// Video not found
+			return;
+		}
+
+		String user = p.getName();
+		if (!tryOperation(v.like(user), response)) {
+			// User has already liked this video
+			return;
+		}
+
+		// All good
+		videos.save(v);
+	}
+
+	// POST /video/{id}/unlike
+	@RequestMapping(value = VIDEO_ID_PATH + "/unlike", method = RequestMethod.POST)
+	public @ResponseBody void unlikeVideo(@PathVariable(ID_PARAM) long id,
+			Principal p, HttpServletResponse response) {
+
+		Video v = getVideoWithResponse(id, response);
+		if (v == null) {
+			// Video not found
+			return;
+		}
+
+		String user = p.getName();
+		if (!tryOperation(v.unlike(user), response)) {
+			// User does not already like this video
+			return;
+		}
+
+		// All good
+		videos.save(v);
+	}
+
 	@RequestMapping(value = VideoSvcApi.VIDEO_TITLE_SEARCH_PATH, method = RequestMethod.GET)
 	public @ResponseBody Collection<Video> findByTitle(
 			@RequestParam(VideoSvcApi.TITLE_PARAMETER) String title) {
+
 		Collection<Video> results = videos.findByName(title);
 		return results;
+
 	}
 
 	@RequestMapping(value = VideoSvcApi.VIDEO_DURATION_SEARCH_PATH, method = RequestMethod.GET)
 	public @ResponseBody Collection<Video> findByDurationLessThan(
 			@RequestParam(VideoSvcApi.DURATION_PARAMETER) long maxDuration) {
+
 		Collection<Video> results = videos.findByDurationLessThan(maxDuration);
+
 		return results;
+	}
+
+	private Video getVideoWithResponse(long id, final HttpServletResponse response) {
+		Video v = videos.findOne(id);
+		if (v == null) {
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+		
+		return v;
+	}
+
+	private boolean tryOperation(boolean result, final HttpServletResponse response) {
+		if (!result) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		}
+		
+		return result;
 	}
 
 }
